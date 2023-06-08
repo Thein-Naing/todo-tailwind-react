6cc9054167ac6d2134833f8b723a7f2c1c5821e1
Thein Naing <theinnaing0908@gmail.com>
2023-06-08 14:55:40 +0800

    edit tailwind.config.js for styling by adding extend: {       colors: {         todo : 'rgba(58, 58, 67, 0.98',         bodyBg :'rgb(32, 32, 42'       },       spacing: {         '2rem': '2rem',         '3rem': '3rem',       },
          fontSize: {
    
            2 : '2rem'
    
          }

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        todo : 'rgba(58, 58, 67, 0.98',
+        bodyBg :'rgb(32, 32, 42'
+      },
+      spacing: {
+        '2rem': '2rem',
+        '3rem': '3rem',
+      },
+
+      fontSize: {
+
+        2 : '2rem'
+
+      }
+    },
   },
   plugins: [],
 }
