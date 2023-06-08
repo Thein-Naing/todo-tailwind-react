ab12a4cf210b1a85adac3bfdfdb5f58906860b8b
Thein Naing <theinnaing0908@gmail.com>
2023-06-08 15:36:03 +0800

    install  npm i react-icons mome nt emoji-mart @emoji-mart/data @emoji-mart/react

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BsEmojiSmile } from 'react-icons/bs'
 
 const Todos = () => {
   return (
     <div className="pt-3rem w-[90%] mx-auto">
-      <h1 className="text-2 font-medium text-center capitalize">Todo List Today</h1>
+      <h1 className="text-2 font-medium text-center capitalize">
+        Todo List Today</h1>
+        {/* 1.todo input */}
+        <div>
+          <form>
+            <div>
+              <textarea cols='30' rows='2'>
+                <span></span>
+
+              </textarea>
+            </div>
+
+          </form>
+        </div>
     </div>
 
    )
