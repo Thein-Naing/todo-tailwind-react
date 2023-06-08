edd4a916eab0efd9f282aed551da57e45a1e7150
Thein Naing <theinnaing0908@gmail.com>
2023-06-08 16:17:22 +0800

    import useState

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import { useState } from "react";
 import { BsEmojiSmile } from "react-icons/bs";
 import { BsPlusSquareFill} from "react-icons/bs";
 import data from '@emoji-mart/data';
 import Picker from '@emoji-mart/react';
 
 const Todos = () => {
+  const [showEmoji, setShowEmoji] = useState(false);
+
+
   return (
     <div className="pt-3rem w-[90%] sm:w-[70%] md:w-[60%] lg:w-[40%] mx-auto">
       <h1 className="text-2 font-medium text-center capitalize">
@@ -24,7 +27,21 @@ const Todos = () => {
             <span className='cursor-pointer hover:text-slate-300'>
               <BsEmojiSmile />
             </span>
+
+          <div className='absolute top-[100%] right-2'>
+            <Picker
+            data = {data}
+            emojiSize={20}
+            emojiButtonSize={30}
+            omEmojiSelect={console.log}
+            maxFrequentRows={0}
+
+            />
+             </div>
           </div>
+
+
+
           <button className='flex items-center capitalize gap-2
           bg-green-100 text-black py-1.5 px-3 rounded-sm hover:bg-blue-200'>
             < BsPlusSquareFill/>Add
