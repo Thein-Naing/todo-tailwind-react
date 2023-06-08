b649c72a3d6d85b1e69079dd82bbf1e8b8da9f21
Thein Naing <theinnaing0908@gmail.com>
2023-06-08 20:42:40 +0800

    updates index.css update Todo.jsx App.js and add Layer.jsx for adding more function updateTodo, deleteTodo e.t.c

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Todos from "./components/Todos";
+import Todo from "./components/Todo";
 
 
 function App() {
   return (
     <div className="App">
       <Todos />
+      <Todo />
     </div>
   );
 }
